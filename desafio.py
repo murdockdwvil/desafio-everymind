f854c1d5003c135f249bd8bab3150e336974d973
beatriz <92122415+murdockdwvil@users.noreply.github.com>
2022-06-23 11:02:00 -0300

    Update desafio.py
    Resumi mais a sessão de login e deixei a mensagem de login mais pessoal.

--- desafio.py
+++ desafio.py
@@ -8,18 +8,19 @@ emailUser = input("Digite seu email: ")
 telefoneUser = int(input("Digite seu número de telefone, com DDD: "))
 senhaUser = input("Digite sua nova senha: ")
 
-if (len(senhaUser) < 6):
-                print("Senha curta demais! Por favor, inclua mais caracteres.")
+if(len(senhaUser) < 6):
+                print("Senha curta demais! Por favor, inclua mais caracteres da próxima vez.")
+                print("Comece o cadrastro novamente.")
 else:
     print("Cadastro completo!")
     print("Por favor, siga para a página de login.")
-
+    
 
 loginUser = input("Digite seu email: ")
 loginSenha = input("Digite a senha cadastrada: ")
 
-if (loginSenha != senhaUser):
-                 print("Senha incorreta! Tente novamente.")
+if(loginUser != emailUser or loginSenha != senhaUser):
+  print("Login não encontrado! Tente novamente.")
 else:
-    print("Login realizado!")
+    print("Login realizado! Bem vinde,",nomeUser)
       
